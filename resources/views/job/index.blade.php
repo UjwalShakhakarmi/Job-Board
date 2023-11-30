289c72b37df384e261011cee070edd33c5301080
UjwalShakhakarmi <ujwalshakha@gmail.com>
2023-11-30 21:24:06 +0545

    Tailwind form installment and starting the search function

--- resources/views/job/index.blade.php
+++ resources/views/job/index.blade.php
@@ -1,6 +1,29 @@
 <x-layout>
     <x-breadcrumbs class="mb-4" 
       :links="['Jobs' => route('jobs.index')]" />
+    <x-card class="mb-4 text-sm">
+      <div class="mb-4 grid grid-cols-2 gap-4">
+        <div>
+          <div class="mb-1 font-semibold">
+              Search
+          </div>
+          <x-text-input name="search" value="" placeholder="Search for any text"/>
+        </div>
+        <div>
+          <div class="mb-1 font-semibold">
+              Salary
+          </div>
+          <div class="flex space-x-2">
+              <x-text-input name="min_salary" value="" placeholder="From"/>
+              <x-text-input name="max_salary" value="" placeholder="To"/>
+          </div>
+        </div>
+        <div>
+
+        </div>
+        <div>4</div>
+      </div>
+    </x-card>
     @foreach ($jobs as $job)
     <!-- card blade ma gayera merge hunxa css sanga  -->
     <x-job-card class="mb-4" :$job>
